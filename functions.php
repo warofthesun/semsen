e695f177fab6338246ab66b4587c31b5a6ed92d8
Dan Brown <fireflip@gmail.com>
2021-03-23 20:48:11 -0500

    Add group link to cart name

--- functions.php
+++ functions.php
@@ -58,73 +58,102 @@ include_once( get_stylesheet_directory() . '/inc/acf/acf.php' );
  //   ));
 //}
 
-/**
-* WooCommerce: show all product attributes listed below each item on Cart page
-*/
-function isa_woo_cart_attributes( $cart_item, $cart_item_key ) {
-    
-    $item_data = $cart_item_key['data'];
-    $attributes = $item_data->get_attributes();
-        
-        
-    if ( ! $attributes ) {
-        return $cart_item;
+// Adding the grouped product ID custom hidden field data in Cart object
+add_filter( 'woocommerce_add_cart_item_data', 'save_custom_fields_data_to_cart', 20, 2 );
+function save_custom_fields_data_to_cart( $cart_item_data, $product_id ) {
+    if( ! empty($_REQUEST['add-to-cart']) && $product_id != $_REQUEST['add-to-cart']
+    && is_numeric($_REQUEST['add-to-cart']) ){
+        $group_prod = wc_get_product($_REQUEST['add-to-cart']);
+        if ( ! $group_prod->is_type( 'grouped' ) )
+            return $cart_item_data; // Exit
+
+        $cart_item_data['grouped_product'] = array(
+            'id' => $_REQUEST['add-to-cart'],
+            'name' => $group_prod->get_name(),
+            'link' => $group_prod->get_permalink(),
+            'visible' => $group_prod->is_visible(),
+        );
+
+        // Below statement make sure every add to cart action as unique line item
+        $cart_item_data['grouped_product']['unique_key'] = md5( microtime().rand() );
     }
-        
-    $out = $cart_item . '<br />';
-       
-    foreach ( $attributes as $attribute ) {
- 
-        // skip variations
-        if ( $attribute->get_variation() ) {
-            continue;
-        }
-        $name = $attribute->get_name();
-        if ( $attribute->is_taxonomy() ) {
-                       
-            $product_id = $item_data->get_id();
-            $terms = wp_get_post_terms( $product_id, $name, 'all' );
- 
-            if ( ! empty( $terms ) ) {
-                if ( ! is_wp_error( $terms ) ) {
- 
-                    // get the taxonomy
-                    $tax = $terms[0]->taxonomy;
- 
-                    // get the tax object
-                    $tax_object = get_taxonomy($tax);
- 
-                    // get tax label
-                    if ( isset ( $tax_object->labels->singular_name ) ) {
-                        $tax_label = $tax_object->labels->singular_name;
-                    } elseif ( isset( $tax_object->label ) ) {
-                        $tax_label = $tax_object->label;
-                        // Trim label prefix since WC 3.0
-                        $label_prefix = 'Product ';
-                        if ( 0 === strpos( $tax_label,  $label_prefix ) ) {
-                            $tax_label = substr( $tax_label, strlen( $label_prefix ) );
-                        }
-                    }
-                    $out .= $tax_label . ': ';
-                    $tax_terms = array();
-                    foreach ( $terms as $term ) {
-                        $single_term = esc_html( $term->name );
-                        array_push( $tax_terms, $single_term );
-                    }
-                    $out .= implode(', ', $tax_terms). '<br />';
- 
-                }
-            }
-              
-        } else {
-          
-            // not a taxonomy 
-              
-            $out .= $name . ': ';
-            $out .= esc_html( implode( ', ', $attribute->get_options() ) ) . '<br />';
-        }
-    }
-    echo $out;
+    return $cart_item_data;
 }
-    
-add_filter( 'woocommerce_cart_item_name', isa_woo_cart_attributes, 10, 2 );
+
+
+// Add the parent grouped product name to cart items names
+add_filter( 'woocommerce_cart_item_name', 'custom_product_title_name', 20, 3 );
+function custom_product_title_name( $cart_item_name, $cart_item, $cart_item_key ){
+    // The product object from cart item
+    $product = $cart_item['data'];
+    $product_permalink = $product->is_visible() ? $product->get_permalink( $cart_item ) : '';
+
+    // The parent product name and data
+    if( isset( $cart_item['grouped_product'] ) ){
+        $group_product = $cart_item['grouped_product'];
+        $group_prod_link = $group_product['link'];
+
+        if ( ! $group_prod_link )
+            return $group_product['name'] . ' > ' . $product->get_name();
+        else
+            return sprintf(
+                '%s <br><a href="%s">%s</a>',
+                $product->get_name(),
+                esc_url( $group_prod_link ),
+                $group_product['name'],
+            );
+    }
+    else
+        return $cart_item_name;
+}
+
+// Save grouped product data in order item meta
+add_action( 'woocommerce_checkout_create_order_line_item', 'added_grouped_order_item_meta', 20, 4 );
+function added_grouped_order_item_meta( $item, $cart_item_key, $values, $order ) {
+    if( isset($values['grouped_product']) ){
+        $item_id = $item->get_id();
+        $grouped_data = $values['grouped_product'];
+        unset($grouped_data['unique_key']);
+        $item->update_meta_data( '_grouped_product', $grouped_data );
+    }
+}
+
+// Display grouped product linked names in order items (+ email notifications)
+add_filter( 'woocommerce_order_item_name', 'custom_order_item_name', 20, 3 );
+function custom_order_item_name( $item_name, $item, $is_visible ) {
+    $product = $item->get_product();
+    $product_id = $item->get_product_id();
+    $product_permalink = $is_visible ? $product->get_permalink( $item ) : '';
+    $grouped_data = wc_get_order_item_meta( $item->get_id(), '_grouped_product', true );
+    if( empty($grouped_data) ){
+        $item_name = $product_permalink ? sprintf(
+            '<a href="%s">%s</a>',
+            esc_url( $product_permalink),
+            $item->get_name()
+        ) : $item->get_name();
+    } else {
+        $item_name = $product_permalink ? sprintf(
+            '<a href="%s">%s</a> > <a href="%s">%s</a>',
+            $grouped_data['link'],
+            $grouped_data['name'],
+            esc_url( $product_permalink) ,
+            $item->get_name()
+        ) : $grouped_data['name'] . ' > ' . $item->get_name();
+    }
+    return $item_name;
+}
+
+// Display on backend order edit pages
+add_action( 'woocommerce_before_order_itemmeta', 'backend_order_item_name_grouped', 20, 3 );
+function backend_order_item_name_grouped( $item_id, $item, $product ){
+    if( ! ( is_admin() && $item->is_type('line_item') ) ) return;
+
+    $grouped_data = wc_get_order_item_meta( $item_id, '_grouped_product', true );
+    if( empty($grouped_data) ) return;
+    $product_link = admin_url( 'post.php?post=' . $grouped_data['id'] . '&action=edit' );
+    $grouped_name_html = '<a href="' . esc_url( $grouped_data['link'] ) . '" class="wc-order-item-name">' . esc_html( $grouped_data['name'] ) . '</a>';
+    echo '<br><br><div class="wc-order-item-name">
+        <small><strong>'.__('Grouped parent').':</strong></small><br>
+        ' . $grouped_name_html . '
+    </div>';
+}
