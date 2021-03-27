ab0f0d0042983e2da3c145eccd22e8a2e76ada3e
Dan Brown <fireflip@gmail.com>
2021-03-27 12:56:33 -0500

    Remove username from Woocommerce account requirements

--- functions.php
+++ functions.php
@@ -179,7 +179,7 @@ function bbloomer_add_name_woo_account_registration() {
     </p>
   
     <p class="form-row form-row-last">
-    <label for="reg_billing_last_name"><?php _e( 'Last name', 'woocommerce' ); ?> </label>
+    <label for="reg_billing_last_name"><?php _e( 'Last name', 'woocommerce' ); ?> <span class="required">*</span></label>
     <input type="text" class="input-text" name="billing_last_name" id="reg_billing_last_name" value="<?php if ( ! empty( $_POST['billing_last_name'] ) ) esc_attr_e( $_POST['billing_last_name'] ); ?>" />
     </p>
   
@@ -196,6 +196,10 @@ add_filter( 'woocommerce_registration_errors', 'bbloomer_validate_name_fields', 
 function bbloomer_validate_name_fields( $errors, $username, $email ) {
     if ( isset( $_POST['billing_first_name'] ) && empty( $_POST['billing_first_name'] ) ) {
         $errors->add( 'billing_first_name_error', __( '<strong>Error</strong>: First name is required!', 'woocommerce' ) );
+    }
+    
+    if ( isset( $_POST['billing_last_name'] ) && empty( $_POST['billing_last_name'] ) ) {
+        $errors->add( 'billing_last_name_error', __( '<strong>Error</strong>: Last name is required!', 'woocommerce' ) );
     }
     return $errors;
 }
